78bd146ddf6d22b141613a163714b5a699b97248
Benjamin Franklin (bf1g25) <bf1g25@soton.ac.uk>
2025-11-24 11:33:20 +0000

    added argument check to function add

--- add.py
+++ add.py
@@ -1,3 +1,5 @@
 def add(x: int, y: int) -> int:
+    if not isinstance(x,int) or not isinstance(y,int):
+        raise TypeError("Args not ints")
     """Returns the sum of two integers."""
     return x + y
